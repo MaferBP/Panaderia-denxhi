3414c27c4070abba9c6944d3c55d60e40e414d1e
MaferBP <l22590318@sjuanrio.tecnm.mx>
2026-05-27 23:09:33 -0600

    Conectar frontend con backend Azure

---
+++ frontend/src/api.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://panaderia-denxhi-api.azurewebsites.net";
+
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const apiUrl = "https://panaderia-denxhi-api.azurewebsites.net"
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/login": "http://127.0.0.1:8000",
-      "/productos": "http://127.0.0.1:8000",
-      "/usuarios": "http://127.0.0.1:8000",
-      "/pedidos": "http://127.0.0.1:8000",
-      "/estadisticas": "http://127.0.0.1:8000",
-      "/ventas-empleado": "http://127.0.0.1:8000",
-      "/stock-bajo": "http://127.0.0.1:8000",
-      "/productos-mas-vendidos": "http://127.0.0.1:8000",
-      "/mermas": "http://127.0.0.1:8000",
-      "/entradas": "http://127.0.0.1:8000",
-      "/inventario": "http://127.0.0.1:8000",
-      "/ventas-fecha": "http://127.0.0.1:8000",
-      "/corte-caja": "http://127.0.0.1:8000",
-      "/cortes-caja": "http://127.0.0.1:8000",
+      "/login": apiUrl,
+      "/productos": apiUrl,
+      "/usuarios": apiUrl,
+      "/pedidos": apiUrl,
+      "/estadisticas": apiUrl,
+      "/ventas-empleado": apiUrl,
+      "/stock-bajo": apiUrl,
+      "/productos-mas-vendidos": apiUrl,
+      "/mermas": apiUrl,
+      "/entradas": apiUrl,
+      "/inventario": apiUrl,
+      "/ventas-fecha": apiUrl,
+      "/corte-caja": apiUrl,
+      "/cortes-caja": apiUrl,
     },
   },
 })
