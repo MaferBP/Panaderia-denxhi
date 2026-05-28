16fef18af3de79c75fbaa44b10e1eb1b6e6c5667
MaferBP <l22590318@sjuanrio.tecnm.mx>
2026-05-28 11:32:58 -0600

    Cambios 4

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,12 @@
 import mysql.connector
 import os
 
-conexion = mysql.connector.connect(
-    host=os.getenv("MYSQL_HOST"),
-    user=os.getenv("MYSQL_USER"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    database=os.getenv("MYSQL_DATABASE"),
-    port=int(os.getenv("MYSQL_PORT")),
-    ssl_disabled=False
-)
+def conectar_db():
+    return mysql.connector.connect(
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DATABASE"),
+        port=int(os.getenv("MYSQL_PORT", "3306")),
+        ssl_disabled=False
+    )
